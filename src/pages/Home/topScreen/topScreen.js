5a1db139e3724df5c39e80d879c507b407eaa143
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-05-27 19:40:32 +0530

    ui fixex

--- src/pages/Home/topScreen/topScreen.js
+++ src/pages/Home/topScreen/topScreen.js
@@ -5,7 +5,8 @@ import img2 from '../../../assets/images/card2.svg'
 import Image from '../../../Components/Image';
 import { CreateForm } from '../../../logic/homeLogic'
 import Card from './card';
-const text1 = `Create a survey`
+const text1 = `Create a survey`;
+const text2 = `Take a Test/Exam`;
  const TopScreen = () => {
      
      return ( 
@@ -13,7 +14,7 @@ const text1 = `Create a survey`
              <Image src={bg} alt="bg" height="100vh" width="100%"/>
              <CardWrapper>
                  <Card img={img1} text = {text1} button="Create Form" handler={CreateForm}/>
-                 <Card img={img2} text = "text" button="btn2"/>
+                 <Card img={img2} text = {text2} button="Create Test" handler={CreateForm} />
              </CardWrapper>
          </SectionWrapper>
       );
