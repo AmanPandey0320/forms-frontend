14f672f3bb81ffea6fb8ac26be63e556026a4649
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-11-20 20:45:20 +0530

    added add option

--- src/formApp/web/pages/form/utility/question.logic.js
+++ src/formApp/web/pages/form/utility/question.logic.js
@@ -1,10 +1,18 @@
+import { questionAction } from "../../../../lib/store/questionSlice";
 import { sectionActions } from "../../../../lib/store/sectoionSlice";
 import store from "../../../../lib/store/store";
 import { http } from "../../../../lib/utils/repository";
-import { saveQuestionToStore } from "../logic";
+import { saveQuestionToStore, saveOptionToStore } from "../logic";
 
 const dispatch = store.dispatch;
 
+/**
+ *
+ * @param {*} sid
+ * @param {*} fid
+ * @param {*} order
+ * @returns
+ */
 export const addNewQuestion = (sid, fid, order) => (e) => {
   const formData = { sid, fid, order };
   http("/api/question/save-action", "POST", { formData })
@@ -29,6 +37,42 @@ export const addNewQuestion = (sid, fid, order) => (e) => {
           id: question.sid,
         })
       );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+/**
+ *
+ * @param {*} qid
+ * @param {*} sid
+ * @param {*} fid
+ */
+export const addNewOption = (qid, sid, fid) => (e) => {
+  const formData = { qid, sid, fid };
+  http("/api/option/save-action", "POST", { formData })
+    .then((res) => {
+      const { data, err, messages } = res.data;
+      if (err.length > 0) {
+        console.log(err);
+        return;
+      }
+      const { result } = data;
+      const { id } = result;
+      const state = store.getState();
+      saveOptionToStore(result);
+      let options = state.question.data[qid]?.options;
+      if (options) {
+        options = [...options, id];
+        dispatch(
+          questionAction.editWithKeyValue({
+            id: qid,
+            key: "options",
+            value: options,
+          })
+        );
+      }
     })
     .catch((err) => {
       console.log(err);
--- src/formApp/web/pages/form/utility/question.utilty.js
+++ src/formApp/web/pages/form/utility/question.utilty.js
@@ -5,8 +5,11 @@ import {
   MdSplitscreen,
   MdImage,
   MdVideoLibrary,
+  MdOutlineAddBox,
+  MdDelete,
+  MdDeleteSweep,
 } from "react-icons/md";
-import { addNewQuestion } from "./question.logic";
+import { addNewQuestion, addNewOption } from "./question.logic";
 
 const QuestionUtilities = ({ data, ...props }) => {
   return (
@@ -14,6 +17,18 @@ const QuestionUtilities = ({ data, ...props }) => {
       <Box style={{ marginTop: "4px", marginBottom: "36px" }}>
         <Paper>
           <Grid container justify="space-evenly" direction="row">
+            {(data?.type === "SO" || data?.type === "MO") && (
+              <Grid item>
+                <Tooltip placement="bottom" title="Add Option">
+                  <IconButton
+                    onClick={addNewOption(data?.id, data?.sid, data?.fid)}
+                    color="primary"
+                  >
+                    <MdOutlineAddBox />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+            )}
             <Grid item>
               <Tooltip placement="bottom" title="Add question">
                 <IconButton
@@ -32,19 +47,33 @@ const QuestionUtilities = ({ data, ...props }) => {
               </Tooltip>
             </Grid>
             <Grid item>
-              <Tooltip placement="bottom" title="Add Section">
+              <Tooltip placement="bottom" title="Add Video">
                 <IconButton color="primary">
                   <MdVideoLibrary />
                 </IconButton>
               </Tooltip>
             </Grid>
             <Grid item>
-              <Tooltip placement="bottom" title="Add Section">
+              <Tooltip placement="bottom" title="Add Image">
                 <IconButton color="primary">
                   <MdImage />
                 </IconButton>
               </Tooltip>
             </Grid>
+            {/* <Grid item>
+              <Tooltip placement="bottom" title="Delete section">
+                <IconButton color="primary">
+                  <MdDeleteSweep />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+            <Grid item>
+              <Tooltip placement="bottom" title="Delete question">
+                <IconButton color="primary">
+                  <MdDelete />
+                </IconButton>
+              </Tooltip>
+            </Grid> */}
           </Grid>
         </Paper>
       </Box>
