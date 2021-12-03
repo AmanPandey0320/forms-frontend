b5b1f6985ba861ef1f57fa7b307dc94e056d7053
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-12-03 22:19:23 +0530

    all response 1/4

--- src/formApp/web/pages/form/response/index.js
+++ src/formApp/web/pages/form/response/index.js
@@ -4,7 +4,19 @@ import {
   Backdrop,
   Snackbar,
   CircularProgress,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  IconButton,
+  Menu,
+  MenuItem,
 } from "@material-ui/core";
+import { AccountCircleTwoTone } from "@material-ui/icons";
+import { HiDotsHorizontal } from "react-icons/hi";
 import { Alert, AlertTitle } from "@material-ui/lab";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -30,6 +42,8 @@ const Response = (props) => {
   const [error, setError] = useState(false);
   const [msg, setMsg] = useState([]);
   const [snackbar, setSnackbar] = useState(false);
+  const [currRes, setCurrRes] = useState(0);
+  const [anchorE1, setAnchorE1] = useState(null);
 
   /** EFFECTS */
   useEffect(() => {
@@ -55,12 +69,21 @@ const Response = (props) => {
   const backDropClickListner = (e) => {
     setSnackbar(true);
   };
+  const openListMenu = (e) => {
+    setAnchorE1(e.currentTarget);
+  };
+  const onCloseListMenu = (e) => {
+    setAnchorE1(null);
+  };
 
   /** LOGS */
   console.log("all forms ------>", allResponse, sentForm, allUid);
 
   /** VIEWS */
 
+  /**
+   * Loading
+   */
   if (loading) {
     return (
       <Backdrop
@@ -82,6 +105,9 @@ const Response = (props) => {
     );
   }
 
+  /**
+   * error
+   */
   if (error) {
     return (
       <Container>
@@ -98,12 +124,92 @@ const Response = (props) => {
       </Container>
     );
   }
+
+  /**
+   * main view
+   */
   return (
-    <Grid container direction="column">
-      <Grid item>
-        <Container>response</Container>
+    <>
+      <Grid container direction="column">
+        <Grid item>
+          <Container>
+            <Grid container direction="column">
+              <Grid item>
+                <Grid container spacing={1} direction="row">
+                  <Grid md={8} item>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Sit illo illum adipisci sapiente mollitia saepe quae nobis,
+                    distinctio voluptatum magni natus iste earum quas, nemo
+                    suscipit itaque delectus excepturi quia hic ipsum. Nobis
+                    neque aut, possimus debitis non enim! Consequatur dolor
+                    dolorum ratione impedit reiciendis corporis fugiat libero
+                    numquam temporibus quidem ab, mollitia maiores! Tempora
+                    repudiandae cum, quod accusantium ducimus, amet nesciunt quo
+                    voluptates ipsam rerum animi? Quasi velit alias libero
+                    molestiae iure et reprehenderit totam. Tempora sequi placeat
+                    dignissimos facere rem minima enim ullam obcaecati
+                    recusandae accusantium. Aut omnis corrupti eligendi labore
+                    fugit nobis tenetur, accusamus adipisci recusandae aperiam.
+                  </Grid>
+                  <Grid md={4} item>
+                    <Grid container direction="column">
+                      <Grid item>
+                        <Paper className={classes.emailDropDownDiv}>
+                          <Grid container direction="column">
+                            <Grid item>
+                              <Typography color="primary">
+                                Respondees
+                              </Typography>
+                            </Grid>
+                            <Grid item>
+                              <List>
+                                {allUid?.map((uid, idx) => (
+                                  <ListItem
+                                    className={classes.emailDropDownListItem}
+                                    key={uid.id}
+                                  >
+                                    {" "}
+                                    <ListItemAvatar>
+                                      <AccountCircleTwoTone color="primary" />
+                                    </ListItemAvatar>{" "}
+                                    <ListItemText
+                                      primary={uid?.name}
+                                      secondary={uid?.email}
+                                    />
+                                    <ListItemSecondaryAction>
+                                      <IconButton
+                                        onClick={openListMenu}
+                                        color="primary"
+                                      >
+                                        <HiDotsHorizontal />
+                                      </IconButton>
+                                    </ListItemSecondaryAction>
+                                  </ListItem>
+                                ))}
+                              </List>
+                            </Grid>
+                          </Grid>
+                        </Paper>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grid>
       </Grid>
-    </Grid>
+      <Menu
+        id="menu-list-action"
+        open={Boolean(anchorE1)}
+        keepMounted
+        anchorEl={anchorE1}
+        onClose={onCloseListMenu}
+      >
+        <MenuItem>Delete</MenuItem>
+        <MenuItem>Download</MenuItem>
+      </Menu>
+    </>
   );
 };
 
--- src/formApp/web/pages/form/response/style.js
+++ src/formApp/web/pages/form/response/style.js
@@ -1,9 +1,27 @@
 import { makeStyles } from "@material-ui/core";
+import { fade } from "@material-ui/core/styles";
 
+/**
+ * material styles
+ */
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: 4,
     color: "#fff",
+  },
+  emailDropDownDiv: {
+    padding: "8px 16px",
+  },
+  emailDropDown: {},
+  emailDropDownListItem: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: "2px 2px 4px 0px grey",
+    borderRadius: "4px",
+    marginBottom: 3,
+    color: theme.palette.common.black,
+    "&:hover": {
+      backgroundColor: fade(theme.palette.primary.main, 0.05),
+    },
   },
 }));
 
